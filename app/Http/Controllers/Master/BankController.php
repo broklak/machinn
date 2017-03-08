dae970ee29ba5dcb403fe37d7e812343294807d0
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2017-03-08 23:10:48 +0530

    add middleware auth to master controller

--- app/Http/Controllers/Master/BankController.php
+++ app/Http/Controllers/Master/BankController.php
@@ -9,6 +9,11 @@ use App\Helpers\GlobalHelper;
 
 class BankController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * @var
      */
